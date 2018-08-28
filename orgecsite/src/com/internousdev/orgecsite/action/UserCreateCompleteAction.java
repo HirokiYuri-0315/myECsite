673bf9aee701a4355eaf84db56e1cc1d24a6f90e
HirokiYuri-0315 <timidhy@gmail.com>
2018-08-28 13:27:07 +0900

    ユーザー登録機能のコード見直しなど

--- orgecsite/src/com/internousdev/orgecsite/action/UserCreateCompleteAction.java
+++ orgecsite/src/com/internousdev/orgecsite/action/UserCreateCompleteAction.java
@@ -10,42 +10,24 @@ import com.opensymphony.xwork2.ActionSupport;
 
 public class UserCreateCompleteAction extends ActionSupport implements SessionAware {
 
-	private String loginUserId;
-	private String loginPassword;
-	private String userName;
 	public Map<String, Object> session;
-	private UserCreateCompleteDAO userCreateCompleteDAO = new UserCreateCompleteDAO();
 
 	public String execute() throws SQLException {
-		userCreateCompleteDAO.createUser(session.get("loginUserId").toString(),		/* cerateUser => createUser */
-				session.get("loginPassword").toString(),
-				session.get("userName").toString());
+		String result = ERROR;
 
-		String result = SUCCESS;
+		if(session.containsKey("loginUserId") && session.containsKey("loginPassword") && session.containsKey("userName")){
+			UserCreateCompleteDAO userCreateCompleteDAO = new UserCreateCompleteDAO();
+			userCreateCompleteDAO.createUser(session.get("loginUserId").toString(),
+					session.get("loginPassword").toString(),
+					session.get("userName").toString());
+			result = SUCCESS;
+		}
+
+		session.remove("loginUserId");
+		session.remove("loginPassword");
+		session.remove("userName");
 
 		return result;
-	}
-
-
-	public String getLoginUserId() {
-		return loginUserId;
-	}
-	public void setLoginUserId(String loginUserId) {
-		this.loginUserId = loginUserId;
-	}
-
-	public String getLoginPassword() {
-		return loginPassword;
-	}
-	public void setLoginPassword(String loginPassword) {
-		this.loginPassword = loginPassword;
-	}
-
-	public String getUsername() {
-		return userName;
-	}
-	public void setUserName(String userName) {
-		this.userName = userName;
 	}
 
 	@Override
